0df8b2604d2e2661bb312ee1723bca099744e575
Mike <92774731+mikethemelon@users.noreply.github.com>
2023-09-03 20:35:39 +0700

    Add files via upload

---
+++ File Changer/File Changer/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace File_Changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.FilesListBox = new System.Windows.Forms.ListBox();
+            this.RemovePrefixButton = new System.Windows.Forms.Button();
+            this.PrefixTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BrowseButton.Location = new System.Drawing.Point(576, 249);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(74, 28);
+            this.BrowseButton.TabIndex = 0;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // FilesListBox
+            // 
+            this.FilesListBox.FormattingEnabled = true;
+            this.FilesListBox.ItemHeight = 16;
+            this.FilesListBox.Location = new System.Drawing.Point(45, 97);
+            this.FilesListBox.Name = "FilesListBox";
+            this.FilesListBox.Size = new System.Drawing.Size(525, 180);
+            this.FilesListBox.TabIndex = 1;
+            // 
+            // RemovePrefixButton
+            // 
+            this.RemovePrefixButton.Location = new System.Drawing.Point(576, 384);
+            this.RemovePrefixButton.Name = "RemovePrefixButton";
+            this.RemovePrefixButton.Size = new System.Drawing.Size(146, 50);
+            this.RemovePrefixButton.TabIndex = 2;
+            this.RemovePrefixButton.Text = "Remove Prefix";
+            this.RemovePrefixButton.UseVisualStyleBackColor = true;
+            this.RemovePrefixButton.Click += new System.EventHandler(this.RemovePrefixButton_Click);
+            // 
+            // PrefixTextBox
+            // 
+            this.PrefixTextBox.Location = new System.Drawing.Point(45, 384);
+            this.PrefixTextBox.Multiline = true;
+            this.PrefixTextBox.Name = "PrefixTextBox";
+            this.PrefixTextBox.Size = new System.Drawing.Size(525, 50);
+            this.PrefixTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "* Insert File(s) need to remove prefix:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 335);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(379, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "* Insert Prefix you want to remove from file(s)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 460);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PrefixTextBox);
+            this.Controls.Add(this.RemovePrefixButton);
+            this.Controls.Add(this.FilesListBox);
+            this.Controls.Add(this.BrowseButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Prefix Changer - Advance Files Rename";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.ListBox FilesListBox;
+        private System.Windows.Forms.Button RemovePrefixButton;
+        private System.Windows.Forms.TextBox PrefixTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ File Changer/File Changer/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace File_Changer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "All Files|*.*",
+                Title = "Select Files"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string filePath in openFileDialog.FileNames)
+                {
+                    FilesListBox.Items.Add(filePath);
+                }
+            }
+        }
+
+        private void RemovePrefixButton_Click(object sender, EventArgs e)
+        {
+            string prefixToRemove = PrefixTextBox.Text.Trim();
+
+            if (string.IsNullOrEmpty(prefixToRemove))
+            {
+                MessageBox.Show("Please enter a prefix to remove.");
+                return;
+            }
+
+            foreach (object item in FilesListBox.Items)
+            {
+                string filePath = item.ToString();
+                string directory = Path.GetDirectoryName(filePath);
+                string fileName = Path.GetFileName(filePath);
+                string newFileName = fileName.StartsWith(prefixToRemove) ? fileName.Substring(prefixToRemove.Length) : fileName;
+                string newFilePath = Path.Combine(directory, newFileName);
+
+                if (File.Exists(newFilePath))
+                {
+                    MessageBox.Show($"A file with the name '{newFileName}' already exists in the directory.");
+                    return;
+                }
+
+                File.Move(filePath, newFilePath);
+            }
+
+            MessageBox.Show($"Prefix '{prefixToRemove}' removed from file names successfully.");
+        }
+
+      
+    }
+}
